7953212cacf3ba427fa0e155c23acef2a1d16eaa
Caner Şahin <caner.sahin@wise-dynamic.com>
2025-07-11 14:52:54 +0300

    .

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['localhost', 'api.example.com'],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://localhost:53759/api/:path*',
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
